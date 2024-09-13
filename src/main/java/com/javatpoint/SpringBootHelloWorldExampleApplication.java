7c65401b5a7f3110d2e5a7c447750dfdbd96028b
abhisaswata008 <abhi.saswata8@gmail.com>
2024-09-13 19:12:46 +0530

    added 2nd comment

--- src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
+++ src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*secondBranch*/
 public class SpringBootHelloWorldExampleApplication
 {
 public static void main(String[] args) 
