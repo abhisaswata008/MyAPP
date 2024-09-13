5af815e0de9905e85ffa7237d8edebc7f3568d0d
abhisaswata008 <50355130+abhisaswata008@users.noreply.github.com>
2024-09-13 19:39:38 +0530

    Update SpringBootHelloWorldExampleApplication.java

--- src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
+++ src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-/*secondBranch*/
+/*secondBranch1234*/
 public class SpringBootHelloWorldExampleApplication
 {
 public static void main(String[] args) 
