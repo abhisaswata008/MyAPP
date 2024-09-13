c1efdebb2be670e85b972b2ebc5301005c712f74
abhisaswata008 <abhi.saswata8@gmail.com>
2024-09-13 17:39:54 +0530

    added comment

--- src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
+++ src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+/*testing*/
 public class SpringBootHelloWorldExampleApplication
 {
 public static void main(String[] args) 
